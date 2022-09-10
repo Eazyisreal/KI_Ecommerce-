b5a2cc5bb7bc7b7ecc40c16e911ab584b45afc12
kassy <kausaratg@gmail.com>
2022-09-10 15:38:45 +0100

    updated

--- E_commerce/account/urls.py
+++ E_commerce/account/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from account import views
+from django.contrib.auth import views as auth_view
 
 
 urlpatterns = [
     path('signup_user/', views.signup_user, name='signup_user'),
     path('authentication_login/', views.authentication_login, name='authentication_login'),
     path('authentication_reset_password/', views.authentication_reset_password, name='authentication_reset_password'),
-    path('logout_user/', views.logout_user, name="logout_user")
+    path('logout_user/', views.logout_user, name="logout_user"),
+
+#     ### password Reset ###
+#     path('reset_password/', auth_view.PasswordResetView.as_view(), name="reset_password"),
+#     path('reset_password_send/', auth_view.PasswordResetDoneView.as_view(), name="password_reset_done"),
+#     path('reset/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+#     path('reset_password_complete/', auth_view.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
--- E_commerce/account/views.py
+++ E_commerce/account/views.py
@@ -4,7 +4,6 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
 
-
 # Create your views here.
 
 
